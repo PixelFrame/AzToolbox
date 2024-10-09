ba1602b4ff501784d8eac55aa1d8487dc432ef6d
Pixel Frame <pm421@live.com>
2024-10-09 14:20:01 +0800

    Support more crypto algorithms

--- AzToolbox/Services/CryptoService.cs
+++ AzToolbox/Services/CryptoService.cs
@@ -5,42 +5,33 @@ using Microsoft.JSInterop;
 
 namespace AzToolbox.Services
 {
-    public class CryptoService
+    public class CryptoService(IJSRuntime JsRuntime)
     {
-        private readonly IJSRuntime JsRuntime;
+        private readonly IJSRuntime JsRuntime = JsRuntime;
 
-        public CryptoService(IJSRuntime JsRuntime)
+        public async Task<string> Aes256Encrypt(string pass, string data, CryptoAlgorithm algo, int length)
         {
-            this.JsRuntime = JsRuntime;
-        }
-
-        private static string HashPassphrase(string pass)
-        {
-            var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(pass));
-            return WebEncoders.Base64UrlEncode(bytes);
-        }
-
-        public async Task<string> Aes256Encrypt(string pass, string data)
-        {
-            return await Aes256Encrypt(pass, Encoding.UTF8.GetBytes(data));
-        }
-
-        public async Task<string> Aes256Encrypt(string pass, byte[] data)
-        {
-            var k = HashPassphrase(pass);
-            var enc = await JsRuntime.InvokeAsync<byte[]>("AesEncrypt", [k, data]);
+            var k = SHA256.HashData(Encoding.UTF8.GetBytes(pass));
+            var algoName = algo.ToString().Replace('_', '-');
+            var dataBytes = Encoding.UTF8.GetBytes(data);
+            var enc = await JsRuntime.InvokeAsync<byte[]>("AesEncrypt", [k, dataBytes, algoName, length]);
             return WebEncoders.Base64UrlEncode(enc);
         }
-        public async Task<string> Aes256Decrypt(string pass, string data)
-        {
-            return await Aes256Decrypt(pass, WebEncoders.Base64UrlDecode(data));
-        }
 
-        public async Task<string> Aes256Decrypt(string pass, byte[] data)
+        public async Task<string> Aes256Decrypt(string pass, string data, CryptoAlgorithm algo, int length)
         {
-            var k = HashPassphrase(pass);
-            var dec = await JsRuntime.InvokeAsync<byte[]>("AesDecrypt", [k, data]);
+            var k = SHA256.HashData(Encoding.UTF8.GetBytes(pass));
+            var algoName = algo.ToString().Replace('_', '-');
+            var dataBytes = WebEncoders.Base64UrlDecode(data);
+            var dec = await JsRuntime.InvokeAsync<byte[]>("AesDecrypt", [k, dataBytes, algoName, length]);
             return Encoding.UTF8.GetString(dec);
         }
+    }
+
+    public enum CryptoAlgorithm
+    {
+        AES_CBC,
+        AES_GCM,
+        AES_CTR
     }
 }
--- AzToolbox/wwwroot/scripts/crypto.js
+++ AzToolbox/wwwroot/scripts/crypto.js
@@ -1,55 +1,100 @@
-﻿ImportAesKey = async function (k) {
+﻿ImportAesKey = async function (key, name) {
     return await window.crypto.subtle.importKey(
-        "jwk",
+        "raw",
+        key,
         {
-            kty: "oct",
-            k: k,
-            alg: "A256CBC",
-            ext: true,
-        },
-        {
-            name: "AES-CBC",
+            name: name,
         },
         false,
         ["encrypt", "decrypt"]
     )
 }
 
-PrependIV = function (iv, data) {
-    const totalLength = iv.byteLength + data.byteLength;
+ImportPbkdf2Key = async function (key) {
+    return await window.crypto.subtle.importKey(
+        "raw",
+        key,
+        {
+            name: "PBKDF2",
+        },
+        false,
+        ["deriveKey", "deriveBits"]
+    )
+}
+
+ConcatBytes = function (iv, salt, data) {
+    const totalLength = iv.byteLength + salt.byteLength + data.byteLength;
     const result = new Uint8Array(totalLength);
 
     result.set(iv, 0);
-    result.set(data, iv.byteLength);
+    result.set(salt, iv.byteLength);
+    result.set(data, iv.byteLength + salt.byteLength);
 
     return result;
 }
 
-
-window.AesEncrypt = async function (k, data) {
-    var key = await ImportAesKey(k);
-    var iv = window.crypto.getRandomValues(new Uint8Array(16));
-    var encrypted = await window.crypto.subtle.encrypt(
+DeriveKey = async function (key, name, salt, len) {
+    return await window.crypto.subtle.deriveKey(
         {
-            name: "AES-CBC",
-            iv: iv,
+            name: "PBKDF2",
+            salt: salt,
+            iterations: 5000,
+            hash: { name: "SHA-256" },
         },
+        key,
+        {
+            name: name,
+            length: len,
+        },
+        false,
+        ["encrypt", "decrypt"]
+    );
+}
+
+window.AesEncrypt = async function (k, data, name, len) {
+    var iv = window.crypto.getRandomValues(new Uint8Array(16));
+    var keySalt = window.crypto.getRandomValues(new Uint8Array(16));
+    var key = await DeriveKey(await ImportPbkdf2Key(k), name, keySalt, len);
+
+    var param = (name == "AES-CTR") ?
+        {
+            name: name,
+            counter: iv,
+            length: 128
+        } :
+        {
+            name: name,
+            iv: iv,
+        };
+
+    var encrypted = await window.crypto.subtle.encrypt(
+        param,
         key,
         data
     );
-    var result = PrependIV(iv, new Uint8Array(encrypted));
+    var result = ConcatBytes(iv, keySalt, new Uint8Array(encrypted));
     return result;
 }
 
-window.AesDecrypt = async function (k, data) {
-    var key = await ImportAesKey(k);
+window.AesDecrypt = async function (k, data, name, len) {
     var iv = data.slice(0, 16);
-    var encrypted = data.slice(16, data.byteLength);
-    var decrypted = await window.crypto.subtle.decrypt(
+    var keySalt = data.slice(16, 32);
+    var encrypted = data.slice(32, data.byteLength);
+
+    var key = await DeriveKey(await ImportPbkdf2Key(k), name, keySalt, len);
+    var param = (name == "AES-CTR") ?
         {
-            name: "AES-CBC",
+            name: name,
+            counter: iv,
+            length: 128
+        } :
+        {
+            name: name,
             iv: iv,
-        },
+        };
+
+    var decrypted = await window.crypto.subtle.decrypt(
+        param,
         key,
         encrypted
     );
