7626c061f63b136b4d96394b29e64757fb3b29df
Pixel Frame <pm421@live.com>
2024-10-21 08:57:06 +0800

    Output type support

--- AzToolbox/Services/CryptoService.cs
+++ AzToolbox/Services/CryptoService.cs
@@ -9,9 +9,11 @@ namespace AzToolbox.Services
     {
         private readonly IJSRuntime JsRuntime = JsRuntime;
 
-        public async Task<string> AesEncrypt(string pass, string data, CryptoAlgorithm algo, int length, int iter)
+        public async Task<string> AesEncrypt(string pass, string data, CryptoAlgorithm algo, int length, int iter, bool plainType)
         {
-            var dataBytes = Encoding.UTF8.GetBytes(data);
+            byte[] dataBytes;
+            if (plainType) dataBytes = Encoding.UTF8.GetBytes(data);
+            else dataBytes = WebEncoders.Base64UrlDecode(data);
             return await AesEncrypt(pass, dataBytes, algo, length, iter);
         }
 
@@ -23,13 +25,14 @@ namespace AzToolbox.Services
             return WebEncoders.Base64UrlEncode(enc);
         }
 
-        public async Task<string> AesDecrypt(string pass, string data, CryptoAlgorithm algo, int length, int iter)
+        public async Task<string> AesDecrypt(string pass, string data, CryptoAlgorithm algo, int length, int iter, bool plainType)
         {
             var k = SHA256.HashData(Encoding.UTF8.GetBytes(pass));
             var algoName = algo.ToString().Replace('_', '-');
             var dataBytes = WebEncoders.Base64UrlDecode(data);
             var dec = await JsRuntime.InvokeAsync<byte[]>("AesDecrypt", [k, dataBytes, algoName, length, iter]);
-            return Encoding.UTF8.GetString(dec);
+            if (plainType) return Encoding.UTF8.GetString(dec);
+            else return WebEncoders.Base64UrlEncode(dec);
         }
     }
 
